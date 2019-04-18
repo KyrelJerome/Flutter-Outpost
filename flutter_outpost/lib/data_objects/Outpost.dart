e47e5102f7050838bcb4a24e44e6035c605eb723
Kyrel Jerome <kyrel2007@gmail.com>
2019-04-17 21:40:41 -0400

    Added Basic Graphics and "Outpost class"

---
+++ flutter_outpost/lib/data_objects/Outpost.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_outpost/template_objects/ReportTemplate.dart';
+
+class Outpost {
+  String name;
+  String label;
+  String organisation;
+  Image icon;
+  List<ReportTemplate> templates;
+  String members;
+  String memberIDs;
+  String host;
+}
